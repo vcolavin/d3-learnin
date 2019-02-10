f7da75be7e76dad3bbf5cec0a801b7fdae44ab72
vcolavin <vcolavin@gmail.com>
2019-02-09 22:31:25 -0800

    eh it's bedtime

--- src/index.tsx
+++ src/index.tsx
@@ -8,24 +8,45 @@ import * as d3 from 'd3';
 const svg = d3.select('svg').style('background-color', 'black');
 const width = parseInt(svg.attr('width'));
 const height = parseInt(svg.attr('height'));
-const n = 20;
+const n = 200;
 const circles = svg
 	.selectAll('circle')
 	.data(d3.range(n))
 	.enter()
 	.append('circle')
 	.attr('r', 10)
-	.attr('fill', d => `rgba(200,200,200,${d / n})`);
+	.attr('fill', d => `rgba(100,${d},200,${d / n})`);
 
-d3.timer(time => {
+const xPeriod: number = 11;
+const yPeriod: number = 10;
+
+// let timerOn = true;
+
+// let currentTime: number;
+
+const timerFunction = (time: number) => {
+	// currentTime = time;
+
 	circles
 		.attr(
 			'cy',
 			d =>
-				(Math.sin((d / 20 + time / 1000) * 2) * height) / 4 + height / 2
+				(Math.sin((d / 200 + time / 1000) * yPeriod) * height) / 4 +
+				height / 2
 		)
 		.attr(
 			'cx',
-			d => (Math.cos(d / 20 + time / 1000) * width) / 4 + width / 2
+			d =>
+				(Math.cos((d / 200 + time / 1000) * xPeriod) * width) / 4 +
+				width / 2
 		);
-});
+};
+
+d3.timer(timerFunction);
+
+// document.addEventListener('click', () => {
+// 	timerOn
+// 		? timer.stop()
+// 		: timer.restart(timerFunction, currentTime, currentTime);
+// 	timerOn = !timerOn;
+// });
